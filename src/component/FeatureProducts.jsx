167c6398f1c59cbd783272684cc54ccc634d0eb4
Rahimul Tahsin <mdrahimultahsin@gmail.com>
2025-11-21 23:45:00 +0600

    add FeatureProduct

---
+++ src/component/FeatureProducts.jsx
@@ -0,0 +1,7 @@
+
+
+export default function FeatureProducts() {
+  return (
+    <div>FeatureProducts</div>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="uppercase font-bold text-5xl">
+     <h1> this is home page</h1>
+    </div>
+  )
+}
